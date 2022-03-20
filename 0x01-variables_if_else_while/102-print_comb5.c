7e2018a0a7b9cf5f9816a313e81a0b19e3129bc1
root <jovanigirma1@gmial.com>
2022-03-20 08:13:38 -0700

    Task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	int c, i, k, j;
 
-	for (c = 48; c <=57; c++)
+	for (c = 48; c <= 57; c++)
 	{
 		for (i = 48; i <= 57; i++)
 		{
@@ -16,7 +16,7 @@ int main(void)
 			{
 				for (j = 48; j <= 57; j++)
 				{
-					if (((k+j) > (c + i) && k >= c) || c < k)
+					if (((k + j) > (c + i) && k >= c) || c < k)
 					{
 						putchar(c);
 						putchar(i);
@@ -24,15 +24,15 @@ int main(void)
 						putchar(k);
 						putchar(j);
 
-						if (c + i + k + j == 227 && c == 57)
-						{
-							break;
-						}
-						else
-						{
-							putchar(',');
-							putchar(' ');
-						}
+					if (c + i + k + j == 227 && c == 57)
+					{
+						break;
+					}
+					else
+					{
+						putchar(',');
+						putchar(' ');
+					}
 					}
 				}
 			}
